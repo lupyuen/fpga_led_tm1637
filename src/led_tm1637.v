8d6fa73240b615a3f4c95680ceb6891441627b97
lupyuen <luppy@appkaki.com>
2018-10-16 03:10:42 +0800

    step id stuck at 2

--- src/led_tm1637.v
+++ src/led_tm1637.v
@@ -60,62 +60,22 @@ reg[0:0] ss; // = 1'b0;  //  Not used for DIO Mode.
 reg[0:0] debug_waiting_for_step_time; // = 1'b0;
 reg[0:0] debug_waiting_for_spi; // = 1'b0;
 
-always@(                //  Code below is always triggered when these conditions are true...
-    posedge clk_led or  //  When the clk_led register transitions from low to high (positive edge) OR
-    negedge rst_n       //  When the reset signal transitions from high to low (negative edge) which
-    ) begin             //  happens when the board restarts or reset button is pressed.
-
-    if (!rst_n) begin     //  If board restarts or reset button is pressed...
-        //clk_led <= 1'b0;
-        cnt2 <= 25'd0;
-
-        //  Init registers here.
-        step_id <= `BLOCK_ROM_INIT_ADDR_WIDTH'h0;
-        /* reg[0:0] */ wait_spi <= 1'b0;
-        /* reg[0:0] */ rd_spi <= 1'b0;
-        /* reg[0:0] */ wr_spi <= 1'b0;
-        /* reg[0:0] */ rst_led <= 1'b0;
-        /* reg[0:0] */ internal_state_machine <= 1'b0;
-        /* reg[27:0] */ elapsed_time <= 28'h0;
-        /* reg[27:0] */ saved_elapsed_time <= 28'h0;
-        /* reg[14:0] */ repeat_count <= 15'h0;
-        /* reg[7:0] */ rx_data <= 8'h0;
-        /* reg[3:0] */ spi_debug <= 4'h0;
-        /* reg[7:0] */ test_display_on <= 8'h8f;
-        /* reg[7:0] */ test_display_off <= 8'h80;
-        /* reg[0:0] */ ss <= 1'b0;  //  Not used for DIO Mode.
-        /* reg[0:0] */ debug_waiting_for_step_time <= 1'b0;
-        /* reg[0:0] */ debug_waiting_for_spi <= 1'b0;
-    end
-    else begin
-        if (cnt2 == 25'd2499_9999) begin  //  If our counter has reached its limit...
-            //clk_led <= ~clk_led;
-            cnt2 <= 25'd0;
-        end
-        else begin
-            cnt2 <= cnt2 + 25'd1;
-        end
-    end
-end
-
 //  switches[3:0] is {1,1,1,1} when all onboard switches {SW4, SW5, SW6, SW7} are in the down position.
 //  We normalise switches[3:0] to {0,0,0,0} such that down=0, up=1.  SW4 is the highest bit, SW7 is the lowest bit.  So {0,0,1,0} becomes value 4'b0010 (decimal 2).
 wire[3:0] normalised_switches = { ~switches[0], ~switches[1], ~switches[2], ~switches[3] };
 
 //  normalised_led[3:0] displays a binary value using onboard LEDs, e.g. it displays {0,0,1,0} when value is 4'b0010 (decimal 2).  {1} means LED On, {0} means LED Off.
 wire[3:0] normalised_led = //  Depending on the onboard switches {SW4, SW5, SW6, SW7}, we show different debug values with the onboard LEDs...
-    (normalised_switches == 4'b0000) ? cnt2[3:0] : ////
-    ////(normalised_switches == 4'b0000) ? step_id :    //  If {SW4,5,6,7}={0,0,0,0}, show the TM1637 ROM step ID that we are executing.
-    (normalised_switches == 4'b0001) ? spi_debug :  //  If {SW4,5,6,7}={0,0,0,1}, show the SPI step ID that we are executing.
-    (normalised_switches == 4'b0010) ? {   //  If {SW4,5,6,7}={0,0,1,0}, 
-        // clk_led[0], ~clk_led[0],           //  show clk_led (left 2 LEDs, {1,0}=High, {0,1}=Low)
-        clk_led[0], 1'b1,
-        // rst_led[0], ~rst_led[0] } :        //  and rst_led (right 2 LEDs, {1,0}=High, {0,1}=Low).
-        1'b1, 1'b1 } :
-    (normalised_switches == 4'b0011) ? {   //  If {SW4,5,6,7}={0,0,1,1}, 
+    (normalised_switches == 4'b0000) ? cnt2[3:0] :  //  If {SW4,5,6,7}={0,0,0,0}, show the cnt2 counter, which is always increasing.
+    (normalised_switches == 4'b0001) ? step_id :    //  If {SW4,5,6,7}={0,0,0,1}, show the TM1637 ROM step ID that we are executing.
+    (normalised_switches == 4'b0010) ? spi_debug :  //  If {SW4,5,6,7}={0,0,1,0}, show the SPI step ID that we are executing.
+    (normalised_switches == 4'b0011) ? {   //  If {SW4,5,6,7}={0,0,1,1},
+        clk_led[0], ~clk_led[0],           //  show clk_led (left 2 LEDs, {1,0}=High, {0,1}=Low)
+        rst_led[0], ~rst_led[0] } :        //  and rst_led (right 2 LEDs, {1,0}=High, {0,1}=Low).
+    (normalised_switches == 4'b0100) ? {   //  If {SW4,5,6,7}={0,1,0,0},
         tm1637_clk[0], ~tm1637_clk[0],     //  show the CLK Pin (left 2 LEDs, {1,0}=High, {0,1}=Low)
         tm1637_dio[0], ~tm1637_dio[0] } :  //  and DIO Pin (right 2 LEDs, {1,0}=High, {0,1}=Low).
-    (normalised_switches == 4'b0100) ? {   //  If {SW4,5,6,7}={0,1,0,0}, 
+    (normalised_switches == 4'b0101) ? {   //  If {SW4,5,6,7}={0,1,0,1}, 
         debug_waiting_for_step_time[0], 
         debug_waiting_for_spi[0],
         1'b1, 1'b1 } :
@@ -143,6 +103,157 @@ wire[0:0] step_rd_spi = encoded_step[1];
 wire[0:0] step_wait_spi = encoded_step[0];
 wire[0:0] tx_buffer_is_empty;
 wire[0:0] charreceived;
+
+always@(                //  Code below is always triggered when these conditions are true...
+    posedge clk_led or  //  When the clk_led register transitions from low to high (positive edge) OR
+    negedge rst_n       //  When the reset signal transitions from high to low (negative edge) which
+    ) begin             //  happens when the board restarts or reset button is pressed.
+
+    if (!rst_n) begin     //  If board restarts or reset button is pressed...
+        //clk_led2 <= 1'b0;
+        cnt2 <= 25'd0;
+
+        //  Init registers here.
+        step_id <= `BLOCK_ROM_INIT_ADDR_WIDTH'h0;
+        /* reg[0:0] */ wait_spi <= 1'b0;
+        /* reg[0:0] */ rd_spi <= 1'b0;
+        /* reg[0:0] */ wr_spi <= 1'b0;
+        /* reg[0:0] */ rst_led <= 1'b0;
+        /* reg[0:0] */ internal_state_machine <= 1'b0;
+        /* reg[27:0] */ elapsed_time <= 28'h0;
+        /* reg[27:0] */ saved_elapsed_time <= 28'h0;
+        /* reg[14:0] */ repeat_count <= 15'h0;
+        /* reg[7:0] */ rx_data <= 8'h0;
+        /* reg[3:0] */ spi_debug <= 4'h0;
+        /* reg[7:0] */ test_display_on <= 8'h8f;
+        /* reg[7:0] */ test_display_off <= 8'h80;
+        /* reg[0:0] */ ss <= 1'b0;  //  Not used for DIO Mode.
+        /* reg[0:0] */ debug_waiting_for_step_time <= 1'b0;
+        /* reg[0:0] */ debug_waiting_for_spi <= 1'b0;
+    end
+    else begin
+        if (cnt2 == 25'd2499_9999) begin  //  If our counter has reached its limit...
+            //clk_led2 <= ~clk_led2;
+            cnt2 <= 25'd0;
+        end
+        else begin
+            cnt2 <= cnt2 + 25'd1;
+        end
+
+        //  If this is not a repeated step...
+        if (!step_should_repeat) begin
+            //  Copy the decoded values into registers so they won't change when we go to next step.
+            //  Values are valid only in next clk_led tick.
+            wr_spi <= step_wr_spi;
+            rd_spi <= step_rd_spi;
+            wait_spi <= step_wait_spi;
+        end
+
+        //  If the step start time has not been reached...
+        if (elapsed_time < step_time) begin
+            debug_waiting_for_step_time <= 1'b1;  //  Waiting for step start time.
+            //  Wait until the step start time has elapsed.
+            elapsed_time <= elapsed_time + 28'h1;
+        end
+        //  If the start time is up and the step is ready to execute...
+        else begin
+            debug_waiting_for_step_time <= 1'b0;  //  Not waiting for step start time.
+            //  Execute the steps for First Tick and Second Tick of clk_led...
+            case (internal_state_machine)
+                //  First Tick: Set up repeating steps.  Don't use wr_spi, rd_spi, wait_spi here because they are only valud in the next clk_led tick.
+                1'b0 : begin
+                    //  If this is a repeating step...
+                    if (step_should_repeat) begin
+                        //  Remember in a register how many times to repeat.
+                        repeat_count <= step_repeat;
+                        //  Remember the elapsed time for recalling later.
+                        saved_elapsed_time <= elapsed_time + 28'h1;
+                    end
+                    //  If this is not a repeating step...
+                    else begin
+                        //  If we are still repeating...
+                        if (repeat_count && step_next > 1) begin
+                            //  Count down number of times to repeat.
+                            repeat_count <= repeat_count - 15'h1;
+                        end
+                    end
+
+                    if (step_rd_spi || step_wr_spi) begin
+                        //  If this is an SPI read or write step, signal to SPI module to start the transfer (rst_led low to high transition).
+                        ////rst_led <= 1'b1;
+                    end
+                    else begin
+                        //  Reset rst_led to low in case we have previously set to high due to SPI read or write step.
+                        ////rst_led <= 1'b0;
+                    end
+
+                    //  Jump to Second Tick step below in the next clock tick.
+                    internal_state_machine <= 1'b1;
+                end
+
+                //  Second Tick: Execute the step.  We can use wr_spi, rd_spi, wait_spi here because this is already the next clk_led tick.
+                1'b1 : begin
+                    //  Reset rst_led to low in case we have previously set to high due to SPI read or write step.
+                    ////rst_led <= 1'b0;
+
+                    //  If we are waiting for SPI command to complete...
+                    if (wait_spi) begin
+                        //  If SPI command has completed...
+                        if (charreceived) begin
+                            debug_waiting_for_spi <= 1'b0;  //  SPI command has just completed.
+                            internal_state_machine <= 1'b0;  //  Will resume at First Tick.
+
+                            //  If we are still repeating...
+                            if (repeat_count) begin
+                                //  Restore the elapsed time.
+                                elapsed_time <= saved_elapsed_time;
+                                //  Jump to the step we should repeat.
+                                if (step_backward)
+                                    step_id <= step_id + ~step_next;
+                                else
+                                    step_id <= step_id + step_next;
+                            end
+                            //  If we are not repeating, or repeat has completed...
+                            else begin
+                                elapsed_time <= elapsed_time + 28'h1;
+                                //  Move to following step.
+                                step_id <= step_id + `BLOCK_ROM_INIT_ADDR_WIDTH'h1;
+                            end
+                        end
+                        //  Else continue waiting for SPI command to complete.
+                        else begin
+                            debug_waiting_for_spi <= 1'b1;  //  Waiting for SPI command to complete.
+                            //  Continue waiting for SPI command to complete.  Will resume at Second Tick.
+                        end
+                    end
+                    //  Else we are not waiting for SPI command to complete...
+                    else begin
+                        debug_waiting_for_spi <= 1'b0;  //  Not waiting for SPI command to complete.
+                        internal_state_machine <= 1'b0;  //  Will resume at First Tick.
+
+                        //  If we are still repeating...
+                        if (repeat_count) begin
+                            //  Restore the elapsed time.
+                            elapsed_time <= saved_elapsed_time;
+                            //  Jump to the step we should repeat.
+                            if (step_backward)
+                                step_id <= step_id + ~step_next;
+                            else
+                                step_id <= step_id + step_next;
+                        end
+                        //  If we are not repeating, or repeat has completed...
+                        else begin
+                            elapsed_time <= elapsed_time + 28'h1;
+                            //  Move to following step.
+                            step_id <= step_id + `BLOCK_ROM_INIT_ADDR_WIDTH'h1;
+                        end
+                    end
+                end
+            endcase
+        end
+
+    end
+end
 
 endmodule
 
@@ -267,17 +378,6 @@ endmodule
             end
             else 
             */
-            //  If this is not a repeated step...
-            if (!step_should_repeat) begin
-                //  Copy the decoded values into registers so they won't change when we go to next step.
-                // oled_vdd <= step_oled_vdd;
-                // oled_vbat <= step_oled_vbat;
-                // oled_res <= step_oled_res;
-                // oled_dc <= step_oled_dc;
-                wr_spi <= step_wr_spi;
-                rd_spi <= step_rd_spi;
-                wait_spi <= step_wait_spi;
-            end
         end
 
         always @ (posedge temp_clk_led)
@@ -297,104 +397,6 @@ endmodule
             ////led <= { ~normalised_led[0], ~normalised_led[1], ~normalised_led[2], ~normalised_led[3] };
             //led <= { ~step_id[0], ~step_id[1], ~step_id[2], ~step_id[3] };
 
-            //  If the step start time has not been reached...
-            if (elapsed_time < step_time) begin
-                debug_waiting_for_step_time <= 1'b1;  //  Waiting for step start time.
-                //  Wait until the step start time has elapsed.
-                elapsed_time <= elapsed_time + 1;
-            end
-            //  If the start time is up and the step is ready to execute...
-            else begin
-                debug_waiting_for_step_time <= 1'b0;  //  Not waiting for step start time.
-                case (internal_state_machine)
-                    //  First Part: Set up repeating steps.
-                    1'b0 : begin
-                        //  If this is a repeating step...
-                        if (step_should_repeat) begin
-                            //  Remember in a register how many times to repeat.
-                            repeat_count <= step_repeat;
-                            //  Remember the elapsed time for recalling later.
-                            saved_elapsed_time <= elapsed_time + 1;
-                        end
-                        //  If this is not a repeating step...
-                        else begin
-                            //  If we are still repeating...
-                            if (repeat_count && step_next > 1) begin
-                                //  Count down number of times to repeat.
-                                repeat_count <= repeat_count - 15'h1;
-                            end
-                        end
-                        //  Handle as a normal step in the next clock tick.
-                        internal_state_machine <= 1'b1;
-
-                        if (rd_spi || wr_spi) begin
-                            //  If this is an SPI read or write step, signal to SPI module to start the transfer (rst_led low to high transition).
-                            rst_led <= 1'b1;
-                        end
-                        else begin
-                            //  Reset rst_led to low in case we have previously set to high due to SPI read or write step.
-                            ////rst_led <= 1'b0;
-                        end
-
-                    end
-                    //  Second Part: Execute the step.
-                    1'b1 : begin
-                        //  Reset rst_led to low in case we have previously set to high due to SPI read or write step.
-                        rst_led <= 1'b0;
-
-                        //  If we are waiting for SPI command to complete...
-                        if (wait_spi) begin
-                            //  If SPI command has completed...
-                            if (charreceived) begin
-                                debug_waiting_for_spi <= 1'b0;  //  SPI command has just completed.
-                                internal_state_machine <= 1'b0;
-                                //  If we are still repeating...
-                                if (repeat_count) begin
-                                    //  Restore the elapsed time.
-                                    elapsed_time <= saved_elapsed_time;
-                                    //  Jump to the step we should repeat.
-                                    if (step_backward)
-                                        step_id <= step_id + ~step_next;
-                                    else
-                                        step_id <= step_id + step_next;
-                                end
-                                //  If we are not repeating, or repeat has completed...
-                                else begin
-                                    elapsed_time <= elapsed_time + 1;
-                                    //  Move to following step.
-                                    step_id <= step_id + `BLOCK_ROM_INIT_ADDR_WIDTH'h1;
-                                end
-                            end
-                            //  Else continue waiting for SPI command to complete.
-                            else begin
-                                debug_waiting_for_spi <= 1'b1;  //  Waiting for SPI command to complete.
-                                //  Continue waiting for SPI command to complete.
-                            end
-                        end
-                        //  Else we are not waiting for SPI command to complete...
-                        else begin
-                            debug_waiting_for_spi <= 1'b0;  //  Not waiting for SPI command to complete.
-                            internal_state_machine <= 1'b0;
-                            //  If we are still repeating...
-                            if (repeat_count) begin
-                                //  Restore the elapsed time.
-                                elapsed_time <= saved_elapsed_time;
-                                //  Jump to the step we should repeat.
-                                if (step_backward)
-                                    step_id <= step_id + ~step_next;
-                                else
-                                    step_id <= step_id + step_next;
-                            end
-                            //  If we are not repeating, or repeat has completed...
-                            else begin
-                                elapsed_time <= elapsed_time + 1;
-                                //  Move to following step.
-                                step_id <= step_id + `BLOCK_ROM_INIT_ADDR_WIDTH'h1;
-                            end
-                        end
-                    end
-                endcase
-            end
         end
 
     `endif  //  NOTUSED
